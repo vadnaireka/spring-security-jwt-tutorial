fa53dcae6bf5d5ceba6aff5e268fba14e2360201
vadnaireka <budaikreativ@gmail.com>
2019-09-06 10:25:46 +0200

    JwtTokenFilter.java

--- src/main/java/com/example/demo/model/UserCredentials.java
+++ src/main/java/com/example/demo/model/UserCredentials.java
@@ -1,4 +1,12 @@
 package com.example.demo.model;
 
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCredentials {
+    private String username;
+    private String password;
 }
