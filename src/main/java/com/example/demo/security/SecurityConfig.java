dae96bb6fce945f5647677aefa544f8e8b10edd9
vadnaireka <budaikreativ@gmail.com>
2019-09-06 10:35:17 +0200

    add new path to config

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers("/auth/signin").permitAll() // allowed by anyone
                     .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated() // allowed only when signed in
+                    .antMatchers(HttpMethod.GET, "/me").authenticated() // allowed only when signed in
                     .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
                     .anyRequest().denyAll() // anything else is denied
                 .and()
